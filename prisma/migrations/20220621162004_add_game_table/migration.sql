132555f199c5ecc6ae2e8fb579fff0c586e56668
xavialfaro <nxb.developer@gmail.com>
2022-06-21 11:35:12 -0500

    add game and room migrations

---
+++ prisma/migrations/20220621162004_add_game_table/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" SERIAL NOT NULL,
+    "difficulty" VARCHAR(20) NOT NULL,
+    "category" VARCHAR(30) NOT NULL,
+    "mode" VARCHAR(20) NOT NULL,
+    "roomId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Game_roomId_key" ON "Game"("roomId");
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
