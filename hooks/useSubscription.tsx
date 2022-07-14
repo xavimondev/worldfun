6ad2c1fa1409a8a8134967231b3598509034617a
xavialfaro <nxb.developer@gmail.com>
2022-07-13 22:38:47 -0500

    improve function to handle insert subscriptions

--- hooks/useSubscription.tsx
+++ hooks/useSubscription.tsx
@@ -1,3 +1,4 @@
+import { SupabaseRealtimePayload } from '@supabase/supabase-js'
 import { useEffect, useState } from 'react'
 import { supabase } from 'services'
 import { searchProfileByUserId } from 'services/profile'
@@ -6,20 +7,23 @@ const useSubscription = () => {
   const [listParticipants, setListParticipants] = useState<any[]>([])
 
   useEffect(() => {
+    const handleInsertSubscription = async (payload: SupabaseRealtimePayload<any>) => {
+      const { new: newParticipant } = payload
+      const { participantId } = newParticipant
+      const profileUser = await searchProfileByUserId(participantId)
+      if (profileUser) {
+        const [infoParticipant] = profileUser
+        console.log('New participant joined the room', infoParticipant)
+        setListParticipants((prevParticipants) => [...prevParticipants, infoParticipant])
+      }
+    }
+
     const subscription = supabase
       .from('RoomParticipants')
-      .on('INSERT', async (payload) => {
-        const { new: newParticipant } = payload
-        const { participantId } = newParticipant
-        const profileUser = await searchProfileByUserId(participantId)
-        if (profileUser) {
-          const [infoParticipant] = profileUser
-
-          console.log('New participant joined the room', infoParticipant)
-          setListParticipants((prevParticipants) => [...prevParticipants, infoParticipant])
-        }
+      .on('INSERT', handleInsertSubscription)
+      .subscribe((status: string) => {
+        console.log(status)
       })
-      .subscribe()
 
     return () => {
       supabase.removeSubscription(subscription)
